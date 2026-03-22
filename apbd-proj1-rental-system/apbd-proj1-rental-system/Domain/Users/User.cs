08529b13d26050a85836f7154010f1a02385939e
s33045 <s33045@pjwstk.edu.pl>
2026-03-22 21:48:26 +0100

    feat: add User model

---
+++ apbd-proj1-rental-system/apbd-proj1-rental-system/Domain/Users/User.cs
@@ -0,0 +1,18 @@
+﻿namespace apbd_proj1_rental_system.Domain.Users;
+
+public abstract class User
+{
+    private static int _nextId = 1;
+
+    public User(string firstName, string lastName)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Id = _nextId++;
+    }
+
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    private int Id { get; set; }
+    public abstract string UserType { get; }
+}
