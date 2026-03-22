3b5809dc7a59b439e479214e6311284352252cb9
s33045 <s33045@pjwstk.edu.pl>
2026-03-22 21:35:06 +0100

    feat: add Projector model

---
+++ apbd-proj1-rental-system/apbd-proj1-rental-system/Domain/Equipment/Projector.cs
@@ -0,0 +1,13 @@
+﻿namespace apbd_proj1_rental_system.Domain.Equipment;
+
+public class Projector : Equipment
+{
+    public Projector(string name, EquipmentStatus status, string resolution, int brightnessLumens) : base(name, status)
+    {
+        Resolution = resolution;
+        BrightnessLumens = brightnessLumens;
+    }
+
+    public string Resolution { get; set; }
+    public int BrightnessLumens { get; set; }
+}
