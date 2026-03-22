6d3f78b787cd6f5bff026757843787d62f85aacf
s33045 <s33045@pjwstk.edu.pl>
2026-03-22 21:37:08 +0100

    feat: add Camera model

---
+++ apbd-proj1-rental-system/apbd-proj1-rental-system/Domain/Equipment/Camera.cs
@@ -0,0 +1,13 @@
+﻿namespace apbd_proj1_rental_system.Domain.Equipment;
+
+public class Camera : Equipment
+{
+    public Camera(string name, EquipmentStatus status, string resolution, string lensType) : base(name, status)
+    {
+        Resolution = resolution;
+        LensType = lensType;
+    }
+
+    public string Resolution { get; set; }
+    public string LensType { get; set; }
+}
