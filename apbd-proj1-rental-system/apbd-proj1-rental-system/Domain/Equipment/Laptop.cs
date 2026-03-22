62d7813c4420887f28c46bf0e91cb4ff5a759494
s33045 <s33045@pjwstk.edu.pl>
2026-03-22 21:32:31 +0100

    feat: add Laptop model

---
+++ apbd-proj1-rental-system/apbd-proj1-rental-system/Domain/Equipment/Laptop.cs
@@ -0,0 +1,13 @@
+﻿namespace apbd_proj1_rental_system.Domain.Equipment;
+
+public class Laptop : Equipment
+{
+    public Laptop(string name, EquipmentStatus status, string processor, int ramGb) : base(name, status)
+    {
+        Processor = processor;
+        RamGb = ramGb;
+    }
+
+    public string Processor { get; set; }
+    public int RamGb { get; set; }
+}
