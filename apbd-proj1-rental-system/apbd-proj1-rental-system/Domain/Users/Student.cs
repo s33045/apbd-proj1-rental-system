cf8548a2ebe9bd1cf8bf43eb135b4c452cbbeee1
s33045 <s33045@pjwstk.edu.pl>
2026-03-22 21:51:46 +0100

    feat: add Student model

---
+++ apbd-proj1-rental-system/apbd-proj1-rental-system/Domain/Users/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace apbd_proj1_rental_system.Domain.Users;
+
+public class Student : User
+{
+    public Student(string firstName, string lastName) : base(firstName, lastName)
+    {
+    }
+
+    public override string UserType => "Student";
+}
