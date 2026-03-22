d36c8ed33ad036700f9e089ad83c98e2e0ec3f9c
s33045 <s33045@pjwstk.edu.pl>
2026-03-22 21:53:03 +0100

    feat: add Employee model

---
+++ apbd-proj1-rental-system/apbd-proj1-rental-system/Domain/Users/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace apbd_proj1_rental_system.Domain.Users;
+
+public class Employee : User
+{
+    public Employee(string firstName, string lastName) : base(firstName, lastName)
+    {
+    }
+
+    public override string UserType => "Employee";
+}
